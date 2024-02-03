6886504260ff6f39bdf4f511db180b056a07fc62
magui165-byte <113001565+magui165-byte@users.noreply.github.com>
2024-02-03 02:38:21 -0500

    Room management basic features completed

--- backend/routes/rooms.js
+++ backend/routes/rooms.js
@@ -26,5 +26,23 @@ router.delete("/:room_id", async (req, res) => {
     res.json("DELETED SUCCESSFULLY");
 })
 
+router.put("/update", async (req, res) => {
+    const roomsUpdate = req.body
+
+    await Rooms.update({
+        room_id: roomsUpdate.room_id,
+        room_type: roomsUpdate.room_type,
+        room_bed_size: roomsUpdate.room_bed_size,
+        room_bed_count: roomsUpdate.room_bed_count,
+        room_view: roomsUpdate.room_view,
+        room_status: roomsUpdate.room_status,
+        room_notes: roomsUpdate.room_notes,
+        room_price: roomsUpdate.room_price}, {
+            where:{
+                room_id: roomsUpdate.room_id,
+            }
+        });
+    res.json(roomsUpdate);
+})
 
 module.exports = router
